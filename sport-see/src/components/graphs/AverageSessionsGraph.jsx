98923db17b3651c91707d7281c4f6e92d2484fe8
Guillaume Bouly <gbouly1@gmail.com>
2025-01-16 15:10:38 +0100

    test nouveau graph

--- sport-see/src/components/graphs/AverageSessionsGraph.jsx
+++ sport-see/src/components/graphs/AverageSessionsGraph.jsx
@@ -1,29 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+import {
+  Line,
+  LineChart,
+  Rectangle,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+} from "recharts";
 import { getUserAverageSessions } from "../../services/api";
-import "./graph.css";
+import "./graph.css"; // Import du fichier CSS existant
 
 // Tooltip personnalisé
 const CustomTooltip = ({ active, payload }) => {
   if (active && payload && payload.length) {
     return (
-      <div className="bg-white text-black text-xs p-1 rounded">
-        <p>{`${payload[0].value} min`}</p>
+      <div className="custom-tooltip">
+        <p className="intro">{`${payload[0].value} min`}</p>
       </div>
     );
   }
   return null;
 };
 
+// Curseur personnalisé
+const CustomCursor = ({ points }) => {
+  const { x } = points[0];
+  return (
+    <Rectangle
+      fill="rgba(0, 0, 0, 0.1)" // Remplissage semi-transparent
+      x={x}
+      y={0}
+      width={100}
+      height={263} // Hauteur totale du graphique
+      opacity={1}
+    />
+  );
+};
+
+// Légende personnalisée
+const CustomLegend = () => (
+  <div className="CustomLegendSession">
+    <h3 className="absolute top-2 left-2 w-[70%] opacity-50">
+      Durée moyenne des sessions
+    </h3>
+  </div>
+);
+
 const AverageSessionsGraph = ({ userId }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchAverageSessions = async () => {
       try {
         const response = await getUserAverageSessions(userId);
 
-        // Reformater les jours pour les abréviations (Lundi, Mardi, ...)
+        // Reformater les jours pour les abréviations
         const daysMap = ["L", "M", "M", "J", "V", "S", "D"];
         const formattedData = response.sessions.map((item, index) => ({
           day: daysMap[index],
@@ -33,60 +65,65 @@ const AverageSessionsGraph = ({ userId }) => {
         setData(formattedData);
       } catch (error) {
         console.error("Failed to fetch average sessions data", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchAverageSessions();
   }, [userId]);
 
+  if (loading) {
+    return <div>Chargement...</div>;
+  }
+
+  if (!data || data.length === 0) {
+    return (
+      <div className="no-data">
+        <p>
+          Les données de l'utilisateur sont indisponibles ou en cours de
+          chargement.
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-[#F00] p-4 rounded shadow h-[250px] w-[250px] relative test">
-      <h2 className="text-white size-4 opacity-50 w-[70%] ">
-        Durée moyenne des sessions
-      </h2>
-      <ResponsiveContainer
-        width="100%"
-        height="100%"
-        className="absolute left-0 bottom-0"
-      >
+    <div className="bg-[#F00]  rounded shadow h-[250px] w-[250px] relative ">
+      {/* Légende personnalisée */}
+      <CustomLegend />
+      <ResponsiveContainer width="100%" height="100%">
         <LineChart
           data={data}
           margin={{
-            top: 100,
+            top: 60,
+            // right: 20,
+            // left: 20,
             bottom: 5,
-            left: 5,
-            right: 0, // Plus de marge latérale
           }}
         >
           {/* Axe X */}
           <XAxis
-            className="x-axis-custom" // Classe pour décaler uniquement les ticks
             dataKey="day"
             tickLine={false}
             axisLine={false}
             tick={{ fill: "#fff", fontSize: 12 }}
-            tickMargin={5}
+            padding={{ left: 10, right: 10 }}
+            opacity={0.5}
           />
 
-          {/* Ligne de progression */}
+          {/* Ligne */}
           <Line
             type="monotone"
             dataKey="sessionLength"
             stroke="#fff"
             strokeWidth={2}
-            dot={{ r: 0 }}
+            dot={false}
             activeDot={{ r: 3 }}
           />
 
-          {/* Tooltip personnalisé */}
-          <Tooltip
-            content={<CustomTooltip />}
-            cursor={{
-              stroke: "rgba(0, 0, 0, 0.2)",
-              strokeWidth: 45,
-              height: 400,
-            }}
-          />
+          {/* Tooltip */}
+          <Tooltip content={<CustomTooltip />} cursor={<CustomCursor />} />
         </LineChart>
       </ResponsiveContainer>
     </div>
